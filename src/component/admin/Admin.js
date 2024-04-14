c7c2fe6fe35c791f42a6742f1a6e518626779fef
bhaveshtaparia <bhaveshtaparia7821@gmail.com>
2024-04-14 20:47:50 +0530

    removing bug 2.0

--- src/component/admin/Admin.js
+++ src/component/admin/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './admin.css'
 import uri from '../../uri';
 import { useNavigate } from 'react-router-dom';
